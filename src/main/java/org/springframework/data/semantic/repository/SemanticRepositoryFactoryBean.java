845832e21fe01ee0db2c890b163bf592d1dc1f13
konstantin.pentchev <konstantin.pentchev@ontotext.com>
2014-06-27 13:27:32 +0300

    implemented SemanticRepositoryBeanFactory

--- src/main/java/org/springframework/data/semantic/repository/SemanticRepositoryFactoryBean.java
+++ src/main/java/org/springframework/data/semantic/repository/SemanticRepositoryFactoryBean.java
@@ -5,13 +5,35 @@ import java.io.Serializable;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
 import org.springframework.data.repository.core.support.TransactionalRepositoryFactoryBeanSupport;
+import org.springframework.data.semantic.repository.support.SemanticRepositoryFactory;
+import org.springframework.data.semantic.support.SemanticTemplateObjectCreator;
+import org.springframework.data.semantic.support.SemanticTemplateStatementsCollector;
 
 public class SemanticRepositoryFactoryBean<T extends Repository<S, ID>, S, ID extends Serializable> extends TransactionalRepositoryFactoryBeanSupport<T, S, ID>{
+	
+	private SemanticTemplateStatementsCollector statementsCollector;
+	
+	private SemanticTemplateObjectCreator objectCreator;
 
 	@Override
 	protected RepositoryFactorySupport doCreateRepositoryFactory() {
-		// TODO Auto-generated method stub
-		return null;
+		return new SemanticRepositoryFactory(statementsCollector, objectCreator);
 	}
+
+	/**
+	 * @param statementsCollector the statementsCollector to set
+	 */
+	public void setStatementsCollector(SemanticTemplateStatementsCollector statementsCollector) {
+		this.statementsCollector = statementsCollector;
+	}
+
+	/**
+	 * @param objectCreator the objectCreator to set
+	 */
+	public void setObjectCreator(SemanticTemplateObjectCreator objectCreator) {
+		this.objectCreator = objectCreator;
+	}
+	
+	
 
 }
