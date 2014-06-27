79741e256deb2512d58e71ab98fb930a39de10f8
Jeen Broekstra <jeen.broekstra@gmail.com>
2014-06-27 12:20:59 +0300

    added unit test for findOne, and resuable vocabulary constants for wine ontology.

--- src/test/java/org/springframework/data/semantic/model/TestSemanticRepository.java
+++ src/test/java/org/springframework/data/semantic/model/TestSemanticRepository.java
@@ -1,22 +1,38 @@
 package org.springframework.data.semantic.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.semantic.core.SemanticDatabase;
+import org.springframework.data.semantic.model.vocabulary.WINE;
+import org.springframework.data.semantic.testutils.Utils;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:META-INF/default-context.xml" })
 public class TestSemanticRepository {
-	
+
 	@Autowired
 	private TestEntityRepository repository;
+
+	@Autowired
+	private static SemanticDatabase sdb;
 	
-	@Test
-	public void test(){
-		
+	@BeforeClass
+	public static void initRepo() {
+		Utils.populateTestRepository(sdb);
 	}
 	
+	@Test
+	public void testFindOne() {
+		TestEntity entity = repository.findOne(WINE.LIGHT);
+		assertNotNull(entity);
+		assertEquals(WINE.LIGHT, entity.getUri());
+	}
 
 }
