a2df76435461922bd19df04445c3d7bd3b3ea420
konstantin.pentchev <konstantin.pentchev@ontotext.com>
2014-06-27 11:47:42 +0300

    semanticRepositoryTest

---
+++ src/test/java/org/springframework/data/semantic/model/TestEntityRepository.java
@@ -0,0 +1,7 @@
+package org.springframework.data.semantic.model;
+
+import org.springframework.data.semantic.repository.SemanticRepository;
+
+public interface TestEntityRepository extends SemanticRepository<TestEntity> {
+
+}
