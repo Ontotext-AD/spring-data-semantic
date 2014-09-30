7f6fa2f259ab2ed0ae908d368da38fa2efa01147
kpentchev <kpentchev@gmail.com>
2014-09-30 13:51:05 +0300

    fix StringToDateConverter for empty values

--- src/main/java/org/springframework/data/semantic/convert/StringToDateConverter.java
+++ src/main/java/org/springframework/data/semantic/convert/StringToDateConverter.java
@@ -4,12 +4,18 @@ import java.util.Date;
 
 import org.openrdf.model.datatypes.XMLDatatypeUtil;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.util.StringUtils;
 
 public class StringToDateConverter implements Converter<String, Date>{
 	
 	@Override
 	public Date convert(String source) {
-		return XMLDatatypeUtil.parseCalendar(source).toGregorianCalendar().getTime();
+		if(!StringUtils.isEmpty(source)){
+			return XMLDatatypeUtil.parseCalendar(source).toGregorianCalendar().getTime();
+		}
+		else {
+			return null;
+		}
 	}
 
 }
