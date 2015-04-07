5b42e5532a7c99fa42b417190b8a2810c1843a08
Atanas Popov <atanas.popov@ontotext.com>
2015-04-07 07:40:26 +0300

    fix indent

--- src/main/java/org/springframework/data/semantic/support/mapping/SemanticMappingContext.java
+++ src/main/java/org/springframework/data/semantic/support/mapping/SemanticMappingContext.java
@@ -43,107 +43,107 @@ import org.springframework.data.util.TypeInformation;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 /**
- * 
+ *
  * @author konstantin.pentchev
  *
  */
 public class SemanticMappingContext  extends AbstractMappingContext<SemanticPersistentEntity<?>, SemanticPersistentProperty>{
-	private static Set<Class<?>> simpleTypes = new HashSet<Class<?>>();
-	static {
-		simpleTypes.add(Value.class);
-		simpleTypes.add(Period.class);
-		simpleTypes.add(Duration.class);
-		simpleTypes.add(Interval.class);
-		simpleTypes.add(Instant.class);
-        simpleTypes.add(XMLGregorianCalendar.class);
-	}
-	
-	private Namespace defaultNS;
-	private Map<String, String> prefix2Namespace;
-	private final boolean explicitSupertypes;
-	
-	public SemanticMappingContext(List<? extends Namespace> namespaces, Namespace defaultNS, boolean explicitSupertypes){
-		super();
-		setSimpleTypeHolder(new SimpleTypeHolder(simpleTypes, true));
-		this.prefix2Namespace = new HashMap<String, String>();
-		for(Namespace ns : namespaces){
-			this.prefix2Namespace.put(ns.getPrefix(), ns.getName());
-		}
-		this.defaultNS = defaultNS;
-		this.explicitSupertypes = explicitSupertypes;
-	}
-	
-	public boolean isSemanticPersistentEntity(Class<?> clazz){
-		return clazz.isAnnotationPresent(SemanticEntity.class);
-	}
-
-	@Override
-	protected <T> SemanticPersistentEntity<T> createPersistentEntity(
-			TypeInformation<T> typeInformation) {
-		final Class<T> type = typeInformation.getType();
-        if (type.isAnnotationPresent(SemanticEntity.class)) {
-            SemanticPersistentEntityImpl<T> persistentEntity = new SemanticPersistentEntityImpl<T>(typeInformation, this);
-            if(this.explicitSupertypes){
-            	List<SemanticPersistentEntity<?>> superTypes = new LinkedList<SemanticPersistentEntity<?>>();
-            	Class<?> supertype = type.getSuperclass();
-            	while(supertype != null){
-            		SemanticPersistentEntity<?> superPersistentEntity = this.getPersistentEntity(supertype);
-            		if(superPersistentEntity != null){
-            			superTypes.add(superPersistentEntity);
-            		}
-            		supertype = supertype.getSuperclass();
-            	}
-            	persistentEntity.setSupertypes(superTypes);
-            }
-            return persistentEntity;
+        private static Set<Class<?>> simpleTypes = new HashSet<Class<?>>();
+        static {
+                simpleTypes.add(Value.class);
+                simpleTypes.add(Period.class);
+                simpleTypes.add(Duration.class);
+                simpleTypes.add(Interval.class);
+                simpleTypes.add(Instant.class);
+                simpleTypes.add(XMLGregorianCalendar.class);
         }
-        throw new IllegalArgumentException("Type " + type + " is not a @SemanticEntity!"); //TODO new Exception type to be used
-	}
 
-	@Override
-	protected SemanticPersistentProperty createPersistentProperty(Field field,
-			PropertyDescriptor descriptor,
-			SemanticPersistentEntity<?> owner,
-			SimpleTypeHolder simpleTypeHolder) {
-		return new SemanticPersistentPropertyImpl(field, descriptor, owner, simpleTypeHolder, this);
-	}
-	
-	/**
-	 * Resolves a {@link String} to an {@link URI}. Check for absolute URI, otherwise try to use defined namespace prefix or use default namespace.
-	 * @param predicate
-	 * @return
-	 */
-	public URI resolveURI(String predicate){
-		if(ValueUtils.isAbsoluteURI(predicate)){
-			return new URIImpl(predicate);
-		}
-		else{
-			String[] parts = predicate.split(":");
-			if(parts.length != 2){
-				if(parts.length == 1){
-					return resolveURIDefaultNS(predicate);
-				}
-				throw new IllegalArgumentException("Not a valid relative URI '"+predicate+"'!");
-			}
-			String ns = prefix2Namespace.get(parts[0]);
-			if(ns == null){
-				throw new IllegalArgumentException("Unknown namespace for prefix '"+parts[0]+"'!");
-			}
-			return new URIImpl(ns+parts[1]);
-		}
-	}
-	
-	/**
-	 * Resolves a {@link String} to an {@link URI} using the default namespace.
-	 * @param lName
-	 * @return
-	 */
-	public URI resolveURIDefaultNS(String lName){
-		return new URIImpl(defaultNS.getName()+lName);
-	}
+        private Namespace defaultNS;
+        private Map<String, String> prefix2Namespace;
+        private final boolean explicitSupertypes;
 
-	@Override public String toString() {
-		// useful for debugging...
-		return String.format("[SemanticMappingContext; default=%s; other=%s]", defaultNS, prefix2Namespace);
-	}
+        public SemanticMappingContext(List<? extends Namespace> namespaces, Namespace defaultNS, boolean explicitSupertypes){
+                super();
+                setSimpleTypeHolder(new SimpleTypeHolder(simpleTypes, true));
+                this.prefix2Namespace = new HashMap<String, String>();
+                for(Namespace ns : namespaces){
+                        this.prefix2Namespace.put(ns.getPrefix(), ns.getName());
+                }
+                this.defaultNS = defaultNS;
+                this.explicitSupertypes = explicitSupertypes;
+        }
+
+        public boolean isSemanticPersistentEntity(Class<?> clazz){
+                return clazz.isAnnotationPresent(SemanticEntity.class);
+        }
+
+        @Override
+        protected <T> SemanticPersistentEntity<T> createPersistentEntity(
+                TypeInformation<T> typeInformation) {
+                final Class<T> type = typeInformation.getType();
+                if (type.isAnnotationPresent(SemanticEntity.class)) {
+                        SemanticPersistentEntityImpl<T> persistentEntity = new SemanticPersistentEntityImpl<T>(typeInformation, this);
+                        if(this.explicitSupertypes){
+                                List<SemanticPersistentEntity<?>> superTypes = new LinkedList<SemanticPersistentEntity<?>>();
+                                Class<?> supertype = type.getSuperclass();
+                                while(supertype != null){
+                                        SemanticPersistentEntity<?> superPersistentEntity = this.getPersistentEntity(supertype);
+                                        if(superPersistentEntity != null){
+                                                superTypes.add(superPersistentEntity);
+                                        }
+                                        supertype = supertype.getSuperclass();
+                                }
+                                persistentEntity.setSupertypes(superTypes);
+                        }
+                        return persistentEntity;
+                }
+                throw new IllegalArgumentException("Type " + type + " is not a @SemanticEntity!"); //TODO new Exception type to be used
+        }
+
+        @Override
+        protected SemanticPersistentProperty createPersistentProperty(Field field,
+                                                                      PropertyDescriptor descriptor,
+                                                                      SemanticPersistentEntity<?> owner,
+                                                                      SimpleTypeHolder simpleTypeHolder) {
+                return new SemanticPersistentPropertyImpl(field, descriptor, owner, simpleTypeHolder, this);
+        }
+
+        /**
+         * Resolves a {@link String} to an {@link URI}. Check for absolute URI, otherwise try to use defined namespace prefix or use default namespace.
+         * @param predicate
+         * @return
+         */
+        public URI resolveURI(String predicate){
+                if(ValueUtils.isAbsoluteURI(predicate)){
+                        return new URIImpl(predicate);
+                }
+                else{
+                        String[] parts = predicate.split(":");
+                        if(parts.length != 2){
+                                if(parts.length == 1){
+                                        return resolveURIDefaultNS(predicate);
+                                }
+                                throw new IllegalArgumentException("Not a valid relative URI '"+predicate+"'!");
+                        }
+                        String ns = prefix2Namespace.get(parts[0]);
+                        if(ns == null){
+                                throw new IllegalArgumentException("Unknown namespace for prefix '"+parts[0]+"'!");
+                        }
+                        return new URIImpl(ns+parts[1]);
+                }
+        }
+
+        /**
+         * Resolves a {@link String} to an {@link URI} using the default namespace.
+         * @param lName
+         * @return
+         */
+        public URI resolveURIDefaultNS(String lName){
+                return new URIImpl(defaultNS.getName()+lName);
+        }
+
+        @Override public String toString() {
+                // useful for debugging...
+                return String.format("[SemanticMappingContext; default=%s; other=%s]", defaultNS, prefix2Namespace);
+        }
 }
